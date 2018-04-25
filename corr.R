af6c8aedf68302b7a87489dddfe6f1e2d6f79be4
Salvador Alejandro Uribe Castellanos <chavagd@hotmail.com>
2018-04-24 23:19:05 -0500

    casos

--- corr.R
+++ corr.R
@@ -9,10 +9,13 @@ corr <- function(directorio,horizonte=0){
     res <- c()
     x <- c()
     for(i in 1:332){
+        
         gato<-formatC(i,width=3,flag="0")
         ana<-paste(gato,"csv",sep=".")
+        
         tab<-na.omit(read.csv(ana))
-        val1<-nrow(tab) 
+        val1<-nrow(tab)
+        
         if(val1>horizonte){
             col2<-data.frame(tab[,2])
             col3<-data.frame(tab[,3])
